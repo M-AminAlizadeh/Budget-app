ac227f99f1d08d1e1963f8e0d587a1ba95a414a4
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-11-28 18:09:11 +0330

    Add splash page contoller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
   get '/sign_out_user', to: 'users#sign_out_user', as: 'sign_out_user'
 
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'devise/sessions' }
+
   devise_scope :user do
     authenticated :user do
       root to: 'dashboard#index', as: :authenticated_root
     end
 
     unauthenticated do
-      root to: 'devise/sessions#new', as: :unauthenticated_root
+      root to: 'splash#index', as: :unauthenticated_root
     end
   end
 end
