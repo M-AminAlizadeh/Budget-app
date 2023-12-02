52a5cfe214b82bfd475c15437bc08d444e19fc8f
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-12-02 22:19:13 +0330

    Add integration test to dashboard controller

---
+++ spec/features/dashboard_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.feature 'Dashboard', type: :feature do
+  scenario 'User can see a list of categories on the dashboard' do
+
+    user = create(:user)
+    login_as(user, scope: :user)
+
+    categories = create_list(:category, 3, user: user)
+
+    visit authenticated_root_path
+
+    expect(page).to have_content('Dashboard')
+
+    within '.categories-list' do
+      categories.each do |category|
+        expect(page).to have_content(category.name)
+        # Add more expectations based on your category display logic
+      end
+    end
+  end
+end
