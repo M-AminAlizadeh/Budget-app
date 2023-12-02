0b903ea191814f4c37867a6528494effa3c223c8
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-12-02 17:58:09 +0330

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :categories
-  has_many :entries, through: :categories
 end
