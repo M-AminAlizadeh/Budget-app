de071ee56871a7c59fa586f18117c80a2b340e6f
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-12-02 23:23:16 +0330

    Add integration test to category controller

--- spec/features/categories_spec.rb
+++ spec/features/categories_spec.rb
@@ -1,40 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe 'Groups Index Page', type: :feature do
-  before do
-    user = User.create(name: 'John Doe', email: 'john@example.com', password: 'password')
-    Group.create(name: 'Category 1', icon: 'icon1.png', user:)
-    Group.create(name: 'Category 2', icon: 'icon2.png', user:)
-
-    visit new_user_session_path
-    fill_in 'Email', with: 'john@example.com'
-    fill_in 'Password', with: 'password'
-    click_button 'Log in'
+RSpec.feature 'Categories', type: :feature do
+  before :each do
+    visit unauthenticated_root_path
   end
 
-  it 'displays categories with their details' do
-    visit groups_path
-
-    expect(page).to have_content('Categories')
-
-    within '.grid' do
-      expect(page).to have_content('Category 1')
-      expect(page).to have_content('Category 2')
-
-      expect(page).to have_content('Icon: icon1.png', count: 1)
-      expect(page).to have_content('Icon: icon2.png', count: 1)
-
-      expect(page).to have_content('Total Amount:', count: 2)
-
-      expect(page).to have_link('View Transactions', count: 2)
-    end
+  it 'has the log in button' do
+    expect(page).to have_text(/log in/i)
   end
 
-  it 'allows adding a new category' do
-    visit groups_path
-
-    click_link 'Add a new category'
-
-    expect(page).to have_content('New Category')
+  it 'has the sign up button' do
+    expect(page).to have_text(/sign up/i)
   end
 end
