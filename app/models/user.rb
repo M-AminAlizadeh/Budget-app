a3819dc6f48faa578727bd765af175113ec845d0
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-12-02 16:49:32 +0330

    Show sum of each category's entries

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :categories
+  has_many :entries, through: :categories
 end
